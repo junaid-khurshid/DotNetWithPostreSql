0669bcb515614d94d259b0b08f6e74715f860792
DESKTOP-FSTC1AE\Juniation <junaid.khursheed@indusvalley.io>
2024-07-22 23:17:00 +0500

    added DbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using DotNetWithPostreSql.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetWithPostreSql.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
