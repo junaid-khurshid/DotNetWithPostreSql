672908abc3fd14efe3375a80382634b239433043
DESKTOP-FSTC1AE\Juniation <junaid.khursheed@indusvalley.io>
2024-07-27 21:14:05 +0500

    Create Post request of Item controller

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,26 @@
+﻿using DotNetWithPostreSql.Data;
+using DotNetWithPostreSql.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetWithPostreSql.Controllers
+{
+    public class ItemController
+    {
+
+        private readonly AppDbContext _context;
+
+        public ItemController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<Item> PostItem(Item item)
+        {
+            _context.Items.Add(item);
+            await _context.SaveChangesAsync();
+
+            return item;
+        }
+    }
+}
